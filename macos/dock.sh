8ac41c75f20024203170ab74b88f2a4d3acd339f
JesseC <jesse.creech@gmail.com>
2020-12-23 11:45:35 +1000

    update dock layout

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
+dockutil --no-restart --add "/Applications/Firefox Developer Edition.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/System/Applications/Calendar.app"
-dockutil --no-restart --add "/System/Applications/Utilities/iTerm.app"
-dockutil --no-restart --add "/System/Applications/System Preferences.app"
+dockutil --no-restart --add "/Applications/Visual Studio Code.app"
+dockutil --no-restart --add "/Applications/GitKraken.app"
+dockutil --no-restart --add "/Applications/Postman.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
+dockutil --no-restart --add "/Applications/iTerm.app"
+dockutil --no-restart --add "/System/Applications/Calendar.app"
+dockutil --no-restart --add "/System/Applications/System Preferences.app"
+
+
